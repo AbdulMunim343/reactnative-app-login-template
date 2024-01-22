5eb08dbad16e3af4ef03180a1e36c07228c9e094
AbdulMunim343 <munimwork90@gmail.com>
2024-01-23 00:10:31 +0500

    updated the dashboard with card and appbar

--- src/components/Background.js
+++ src/components/Background.js
@@ -1,18 +1,16 @@
 import React from 'react'
-import { ImageBackground, StyleSheet, KeyboardAvoidingView } from 'react-native'
+import { ImageBackground, StyleSheet, KeyboardAvoidingView,View } from 'react-native'
 import { theme } from '../core/theme'
 
 export default function Background({ children }) {
   return (
-    // <ImageBackground
-    //   source={require('../assets/background_dot.png')}
-    //   resizeMode="repeat"
-    //   style={styles.background}
-    // >
+     <View
+      style={styles.background}
+    >
       <KeyboardAvoidingView style={styles.container} behavior="padding">
         {children}
       </KeyboardAvoidingView>
-    //</ImageBackground>
+    </View>
   )
 }
 
@@ -24,9 +22,9 @@ const styles = StyleSheet.create({
   },
   container: {
     flex: 1,
-    padding: 20,
+    padding: 10,
     width: '100%',
-    //maxWidth: 340,
+    maxWidth: 340,
     alignSelf: 'center',
     alignItems: 'center',
     justifyContent: 'center',
--- src/screens/Dashboard.js
+++ src/screens/Dashboard.js
@@ -1,30 +1,35 @@
-import React from 'react'
-import Background from '../components/Background'
-import Logo from '../components/Logo'
-import Header from '../components/Header'
-import Paragraph from '../components/Paragraph'
-import Button from '../components/Button'
+import React from 'react';
+import { View,StyleSheet } from 'react-native';
+import { Card, Text, Appbar } from 'react-native-paper';
+
 
 export default function Dashboard({ navigation }) {
   return (
-    <Background>
-      <Logo />
-      <Header>Let’s start</Header>
-      <Paragraph>
-        Your amazing app starts here. Open you favorite code editor and start
-        editing this project.
-      </Paragraph>
-      <Button
-        mode="outlined"
-        onPress={() =>
-          navigation.reset({
-            index: 0,
-            routes: [{ name: 'StartScreen' }],
-          })
-        }
-      >
-        Logout
-      </Button>
-    </Background>
+    <>
+      <Appbar.Header>
+        <Appbar.Action icon="arrow-left" onPress={() => { }} />
+        <Appbar.Content title="List" />
+      </Appbar.Header>
+      <View style={styles.container}>
+        <Card>
+        <Card.Content>
+          <Text variant="titleLarge">Card title</Text>
+          <Text variant="bodyMedium">Card content</Text>
+        </Card.Content>
+      </Card>
+      </View>
+      
+    </>
+
+
+
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 10,
+    width: '100%',
+  },
+})
