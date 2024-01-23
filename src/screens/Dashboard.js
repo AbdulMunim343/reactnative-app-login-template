fa9219a955a6fda1b75be295f7a783c48acf5394
AbdulMunim343 <munimwork90@gmail.com>
2024-01-24 00:36:39 +0500

    rendered the card from API also used axios

--- src/screens/Dashboard.js
+++ src/screens/Dashboard.js
@@ -1,35 +1,53 @@
-import React from 'react';
-import { View,StyleSheet } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { View, StyleSheet } from 'react-native';
 import { Card, Text, Appbar } from 'react-native-paper';
+import axiosConfig from '../configs/axiosConfig';
 
 
 export default function Dashboard({ navigation }) {
+
+  const [data, setData] = useState([])
+  useEffect(() => {
+    getData();
+  }, [])
+
+  const getData = () => {
+    axiosConfig.get('/users').then((res) => {
+      console.log(res);
+      setData(res.data)
+    }).catch((error) => {
+      console.log(error)
+    })
+  }
+
   return (
     <>
       <Appbar.Header>
-        <Appbar.Action icon="arrow-left" onPress={() => { }} />
-        <Appbar.Content title="List" />
+        <Appbar.Content title="Dashboard" />
       </Appbar.Header>
       <View style={styles.container}>
-        <Card>
-        <Card.Content>
-          <Text variant="titleLarge">Card title</Text>
-          <Text variant="bodyMedium">Card content</Text>
-        </Card.Content>
-      </Card>
+        {data.map((res) => {
+            return(
+              <Card style={styles.list}>
+              <Card.Content>
+                <Text variant="titleMedium">{res.name}</Text>
+              </Card.Content>
+            </Card>
+            )
+        })}
       </View>
-      
     </>
-
-
-
   )
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    padding: 10,
+    padding: 5,
     width: '100%',
   },
+
+  list:{
+    marginBottom:10
+  }
 })
